8bccb0f53e045469455f83e68731e94f0c7dd280
jquie <jacquelynphan03@gmail.com>
2025-03-09 01:08:34 +0000

    done

--- Main.java
+++ Main.java
@@ -6,26 +6,48 @@ public class Main
   {
     int[][] arrInt = {{8,7,5,8},{1,3,9,5},{3,4,6,2}};
     System.out.println(sumOfDiag(arrInt));
-  }
 
+    int rows = 6;
+    int cols = 5;
+    int[][] table = productTable(rows, cols);
+    for (int i = 0; i < rows; i++) 
+    {
+      for (int j = 0; j < cols; j++) 
+      {
+        System.out.print(table[i][j]);
+        System.out.print("  ");
+      }
+    System.out.println();
+    System.out.println("----------------");
+    }
+  }
 
   public static int sumOfDiag(int[][] matrix)
   {
     int sum = 0;
-    for (int inner = 0; inner < matrix[0].length; inner++)
+    for (int row = 0; row < matrix.length; row++)
     {
-      for (int outer = 0; outer < matrix.length; outer++)
+      for (int col = 0; col < matrix[0].length; col++)
       {
-        sum += matrix[outer][inner];
+        if(row == col)
+        sum += matrix[row][col];
       }
     }
     
     return sum;
   }
 
-/*  public static int[][] productTable(final int numRows, final int numCols)
+  public static int[][] productTable(final int numRows, final int numCols)
   {
-    return null;
+    int[][] table = new int[numRows][numCols];
+    for (int i = 0; i < numRows; i++)
+    {
+      for (int j = 0; j < numCols; j++) 
+        {
+          table[i][j] = i * j;
+        }
+    }
+    return table;
   }
-  */
+
 }
