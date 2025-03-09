62d7c994d2838da82ed689df21c1fd6b6dc92ee1
jquie <jacquelynphan03@gmail.com>
2025-03-09 00:08:15 +0000

    not done

--- Main.java
+++ Main.java
@@ -4,17 +4,28 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Test code here
+    int[][] arrInt = {{8,7,5,8},{1,3,9,5},{3,4,6,2}};
+    System.out.println(sumOfDiag(arrInt));
   }
 
 
   public static int sumOfDiag(int[][] matrix)
   {
-    return 0;
+    int sum = 0;
+    for (int inner = 0; inner < matrix[0].length; inner++)
+    {
+      for (int outer = 0; outer < matrix.length; outer++)
+      {
+        sum += matrix[outer][inner];
+      }
+    }
+    
+    return sum;
   }
 
-  public static int[][] productTable(final int numRows, final int numCols)
+/*  public static int[][] productTable(final int numRows, final int numCols)
   {
     return null;
   }
+  */
 }
